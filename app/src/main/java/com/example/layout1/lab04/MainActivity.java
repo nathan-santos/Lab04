b6b9cb4bca0d08759d5ebf09925a9d8c9d305781
Nathan Santos <nathanemail.ns@gmail.com>
2015-07-08 14:57:31 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/layout1/lab04/MainActivity.java
+++ app/src/main/java/com/example/layout1/lab04/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+/**
+* @author Nathan Santos - 08/07/2015
+*/
 
 public class MainActivity extends ActionBarActivity {
 
